9f14a8a9de84b74d60ad2daf3eab522794f87779
Yili Yu <td821211@gmail.com>
2022-08-02 23:37:54 -0500

    Update train.py

--- gan/train.py
+++ gan/train.py
@@ -41,20 +41,6 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
-def adjust_learning_rate(optimizer, epoch, args):
-    epoch += 1
-    if epoch <= args.warmup:
-        lr = args.learning_rate * epoch / args.warmup
-    elif epoch > args.steps[1]:
-        lr = args.learning_rate * 0.01
-    elif epoch > args.steps[0]:
-        lr = args.learning_rate * 0.1
-    else:
-        lr = args.learning_rate
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-
-
 
 def train(device, epoch, data_loader, Generater, Discriminator, Goptimizer, Doptimizer, criterion1, criterion2):
     """
